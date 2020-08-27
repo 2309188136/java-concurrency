b103ec7d2c914fd78ac90639d2dd40e3e727b232
2309188136 <2309188136@qq.com>
2020-08-27 00:41:01 -0500

    partially written ExchangeExample1

--- jcuexample/src/test/java/juc/utils/ExchangeExample1.java
+++ jcuexample/src/test/java/juc/utils/ExchangeExample1.java
@@ -14,6 +14,17 @@ public class ExchangeExample1 {
                 e.printStackTrace();
             }
             System.out.println(Thread.currentThread().getName() + " completed!");
-        }).start();
+        },"A").start();
+
+        new Thread(()->{
+            System.out.println(Thread.currentThread().getName() + " started");
+            try {
+                String result = exchanger.exchange("Message from " + Thread.currentThread().getName());
+                System.out.println(Thread.currentThread().getName() + " got result value: " + result);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " completed!");
+        },"B").start();
     }
 }
